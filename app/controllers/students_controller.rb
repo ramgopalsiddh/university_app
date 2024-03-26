6041c6e2e268480ffa2f88512c119430a6a7e068
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-27 01:17:26 +0530

    Day 63 Add create method  & error message in new form for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,20 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You have successfully signed up"
+      redirect_to root_path
+    else
+        render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+
 end
