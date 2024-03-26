94c90d8bb49e6ba7cda737125cc025b29b7a95d5
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-26 18:21:06 +0530

    Day 62 Create students resource (db table & model )

---
+++ app/models/student.rb
@@ -0,0 +1,5 @@
+class Student < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGIX }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_091313) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_122714) do
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
