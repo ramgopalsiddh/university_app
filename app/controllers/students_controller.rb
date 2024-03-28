bd6d471da6ae0db6c5004c159973a102f02baa88
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-28 22:14:19 +0530

    Day 64  Student Show Page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,10 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
     @student = Student.new
   end
