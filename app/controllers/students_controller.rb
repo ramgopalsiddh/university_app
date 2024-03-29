87981df17999a96b7e5e4e09db485b1424fe65a1
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-29 21:29:19 +0530

    Day 65 Add edit student method and icon

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,6 +22,20 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:success] = "You have successfully updated your profile"
+      redirect_to student_path(@student)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def student_params
