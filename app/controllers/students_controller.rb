bcc6e8cdd3614a9a8f1cfb56e91c9fcc337a0ae0
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-30 20:34:41 +0530

    Day 66 Extract redundancies ( by using  partials)

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
 
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -16,18 +16,16 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:success] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to @student
     else
         render 'new', status: :unprocessable_entity
     end
   end
 
   def edit
-    @student = Student.find(params[:id])
   end
 
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:success] = "You have successfully updated your profile"
       redirect_to student_path(@student)
@@ -37,6 +35,10 @@ class StudentsController < ApplicationController
   end
 
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
