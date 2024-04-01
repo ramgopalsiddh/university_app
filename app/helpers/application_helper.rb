7a1bc4049d6076523f5f5ede9c2fdc2e5c23abea
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-01 21:25:34 +0530

    Day 68 Add login and logout buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def session_link
+    if (logged_in?)
+      link_to('Log out', logout_path, data: { turbo_method: :delete })
+    else
+      link_to("Log in", login_path)
+    end
+  end
 end
